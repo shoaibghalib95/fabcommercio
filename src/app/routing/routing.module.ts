8642a34c6d23bc7631eeb13eab29109caf77d41c
shoaibghalib <shoaib.ghalib@hotmail.com>
2018-08-17 05:06:27 +0500

    home page

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -7,7 +7,6 @@ import { CommonModule } from '@angular/common';
 
 
 const routes: Routes = [
-  // { path: '', component: HomeComponent},
   { path: 'home', component: HomeComponent },
   { path: 'products', component: ProductsComponent },
   { path: 'contactUs', component: ContactComponent },
